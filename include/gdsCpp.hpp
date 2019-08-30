0818c2b1ac0846b67ad061de21642482197da75f
judefdiv <47555862+judefdiv@users.noreply.github.com>
2019-08-30 16:02:46 +0200

    Reverting to old code. (#7)

--- include/gdsCpp.hpp
+++ include/gdsCpp.hpp
@@ -14,12 +14,19 @@
 
 using namespace std;
 
-class gdsSTR;
-class gdsBOUNDARY;
-class gdsPATH;
-class gdsNODE;
-class gdsSREF;
-class gdsTEXT;
+// ==================== Class declarations =====================
+class gdscpp;                                  // All encompassing master class
+class gdsInfo;                                 // Subclass of gds file
+class gdsSTR;                                  // Subclass containing all structures
+class gdsBOUNDARY;                             // x2subclass belonging to gdsSTR
+class gdsPATH;                                 // ''
+class gdsSREF;                                 // ''
+class gdsAREF;                                 // ''
+class gdsTEXT;                                 // ''
+class gdsNODE;                                 // ''  
+class gdsBOX;                                  // ''  
+
+// ========================== Includes ========================
 
 #include <string>
 #include <iostream>
@@ -32,10 +39,22 @@ class gdsTEXT;
 #include "gdsForge.hpp"
 #include "gdsImport.hpp"
 
+// ===================== Class Definitions ====================
+/*
+This is the highest class, holding all the information of a single .gds file.
+*/
 class gdscpp{  // (GDS file)
   private:
-    vector<gdsSTR> STR;
-    int GDSrecord2ASCII(char *recIn); // Does it belong here???
+    vector<gdsSTR> STR;                       // Holds all the structures of the gds file
+
+  int version_number=7;                     // GDS version number. Default to 7
+    int last_modified[6] = {0,0,0,0,0,0};     // Default to current datetime if unread
+    int last_accessed[6] = {0,0,0,0,0,0};     // Default to current datetime if unread
+    string library_name = "Untitled_library"; // Default libname
+    int generations = 3;                      // Default generations. Don't really use
+    double units[2] = {0.001, 1e-09};         // micron default 
+
+    int GDSrecord2ASCII(char *recIn);         // Does it belong here??? 
 
   public:
     gdscpp();
@@ -47,10 +66,14 @@ class gdscpp{  // (GDS file)
     int read(string fileName);
     int write(string fileName);
 
-    int quick2ASCII(string fileName);   // does not store data, legacy code.
+    int quick2ASCII(string fileName);       // does not store data, legacy code.
 
     void to_str();
 };
+
+/*
+Subclass of gdscpp which holds a structure's information.
+*/
 
 class gdsSTR{
   private:
@@ -62,13 +85,67 @@ class gdsSTR{
     void to_str();
 
     string name = "\0";
-    vector<gdsSREF> SREF;
+    
     vector<gdsBOUNDARY> BOUNDARY;
     vector<gdsPATH> PATH;
-    vector<gdsNODE> NODE;
+    vector<gdsSREF> SREF;
+    vector<gdsAREF> AREF;
     vector<gdsTEXT> TEXT;
-
+    vector<gdsNODE> NODE;
+    vector<gdsBOX> BOX;
+    
 };
+
+/*
+Subclass of a gdsSTR class which stores boundary structures.
+*/
+class gdsBOUNDARY{
+  private:
+
+  public:
+    gdsBOUNDARY();
+    ~gdsBOUNDARY(){};
+
+    void to_str();
+
+    unsigned int layer = 0;
+    unsigned int dataType = 0;
+    vector<int> xCor;
+    vector<int> yCor;
+    unsigned int propattr = 0;
+    string propvalue = "\0";
+};
+
+/*
+Subclass of a gdsSTR class which stores path structures.
+
+Pathtype: This record contains a value that describes the type of path endpoints. The value is
+0 for square-ended paths that endflush with their endpoints
+1 for round-ended paths
+2 for square-ended paths that extend a half-width beyond their endpoints
+*/
+class gdsPATH{
+  private:
+
+  public:
+    gdsPATH();
+    ~gdsPATH(){};
+
+    void to_str();
+
+    unsigned int layer = 0;
+    unsigned int dataType = 0;
+    unsigned int pathtype = 0;
+    unsigned int width = 0;
+    vector<int> xCor;
+    vector<int> yCor;
+    unsigned int propattr = 0;
+    string propvalue = "\0";
+};
+
+/*
+Subclass of a gdsSTR class which stores structure reference structures.
+*/
 
 class gdsSREF{
   private:
@@ -85,6 +162,58 @@ class gdsSREF{
     double scale = 1;
     int xCor;
     int yCor;
+    unsigned int propattr = 0;
+    string propvalue = "\0";
+};
+
+/*
+Subclass of a gdsSTR class which stores array reference structures.
+*/
+class gdsAREF{
+  private:
+
+  public:
+    gdsAREF();
+    ~gdsAREF(){};
+
+    void to_str();
+
+                              // PLEX
+    string name = "\0";       // SNAME
+    bool reflection = false;  // STRANS
+    double angle = 0;         //  subSTRANS
+    double scale = 1;         //  subSTRANS
+    int xCor;                 // XY
+    int yCor;                 // XY
+    unsigned int propattr = 0;
+    string propvalue = "\0";
+};
+
+/*
+Subclass of a gdsSTR class which stores text structures.
+*/
+class gdsTEXT{
+  private:
+
+  public:
+    gdsTEXT();
+    ~gdsTEXT(){};
+
+    void to_str();
+
+    string textbody = "\0";
+    unsigned int layer = 0;
+    double scale = 1;
+    int xCor;
+    int yCor;
+    unsigned int propattr = 0;
+    string propvalue = "\0";
+};
+
+/*
+Subclass of a gdsSTR class which stores node structures.
+*/
+
 };
 
 class gdsBOUNDARY{
@@ -137,6 +266,28 @@ class gdsNODE{
     string propvalue = "\0";
 };
 
+/*
+Subclass of a gdsSTR class which stores box structures.
+*/
+class gdsBOX{
+  private:
+
+  public:
+    gdsBOX();
+    ~gdsBOX(){};
+
+    void to_str();
+
+    unsigned int plex = 0;  // optional
+    unsigned int layer = 0;
+    unsigned int boxtype = 0;
+    // A maximum of 50 coordinates
+    vector<int> xCor;
+    vector<int> yCor;
+    unsigned int propattr = 0;
+    string propvalue = "\0";
+};
+  
 class gdsTEXT{
   private:
 
