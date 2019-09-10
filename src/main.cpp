8ba2cc54e07d2dde3c1a5be09f532df8cebfcf47
Heinrich <heinhfh@gmail.com>
2019-09-10 10:55:27 +0200

    Premerge 2

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ int RunTool(int argCount, char** argValues){
 		if(gdsFName.compare("\0")){
 			gdscpp gdsfile;
 			gdsfile.import(gdsFName);				//Todo - comment in import
-			gdsfile.write("Heinsfile.txt");
+			// gdsfile.write("Heinsfile.gds");
 			return 1;
 		}
 		else{
